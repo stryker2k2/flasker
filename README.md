d7c5489ff5087bf1e5606d7fbf0ae3548fe87e4d
Jack Ward <stryker2k2@msn.com>
2023-04-21 13:39:53 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Flasker
+
+An online blog template created using Flask from YouTube Tutorial series called "Create A Flask Blog - Flask Friday"
+https://youtu.be/0Qxtt4veJIc
+
+### Setup
+- python3 -m venv virt
+- source virt/bin/activate
+- deactivate (optional)
+- pip install grip (view README.md locally)
+- pip install flask
+- pip freeze (to view install python modules)
+- export FLASK_DEBUG=True
+- export FLASK_APP=hello.py
+- flask run
+
+### Daily Use
+- source virt/bin/activate
+- flask run 
+- grip (view README.md locally)
+
+### Other Commands
+- echo $FLASK_ENV
+- unset FLASK_ENV
+
---
+++ hello.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+# Create a Flask Instance
+app = Flask(__name__)
+
+# Create a Route Decorator
+@app.route('/')
+def index():
+    first_name = 'John'
+    bold_stuff = 'This is <strong>BOLD</strong> Text'
+    lower_stuff = 'this is bold text'
+    fav_pizza = ['Pepperoni', 'Cheese', 'Mushroom', 41]
+    return render_template('index.html', 
+        user_name = first_name,
+        stuff = bold_stuff,
+        fav_pizza = fav_pizza)
+
+# localhost:5000/user/john
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', user_name = name)
+
+
+# Custom Error Page(s)
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
