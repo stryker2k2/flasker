de74d75b0b7d7102425a4cce6ba32f2a79c652f6
Jack Ward <stryker2k2@msn.com>
2023-05-12 14:30:20 -0500

    Added About Author section

--- hello.py
+++ hello.py
@@ -83,6 +83,7 @@ def dashboard():
         name_to_update.email = request.form['email']
         name_to_update.fav_color = request.form['fav_color']
         name_to_update.username = request.form['username']
+        name_to_update.about_author = request.form['about_author']
         try:
             db.session.commit()
             flash("User Updated Successfully")
@@ -415,6 +416,7 @@ class Users(db.Model, UserMixin):
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     fav_color = db.Column(db.String(120))
+    about_author = db.Column(db.Text(500), nullable=True)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
     password_hash = db.Column(db.String(128))
     # User can have many posts - need database relationship
